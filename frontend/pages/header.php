02a2cb5d95099898c01b43c088a5e9c5a48c3bf6
enricopaolotoso <74261261+enricopaolotoso@users.noreply.github.com>
2023-03-17 18:50:27 +0100

    Create pages file
    Create all pages php with html.
    Create header.

---
+++ frontend/pages/header.php
@@ -0,0 +1,23 @@
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="amministratore/amministratore.html">League</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="calendario.php">Match</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="regole.php">Player</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="squadre.php">Formation</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="medie.php">Squad</a>
+              </li><li class="nav-item">
+                <a class="nav-link" href="classifica.php?BY=punti">User</a>
+              </li>
+          </ul>
+        </div>
+      </nav>
