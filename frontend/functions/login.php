712dcb7e969334e5d69676366b2de80758660377
enricopaolotoso <74261261+enricopaolotoso@users.noreply.github.com>
2023-03-17 18:48:13 +0100

    Create login.php

---
+++ frontend/functions/login.php
@@ -0,0 +1,37 @@
+<?php 
+
+function login($data)
+    {
+        $url = 'http://localhost/Fantacalcio-main/Backend/api/user/login.php';
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url); // setta l'url
+        curl_setopt($curl, CURLOPT_POST, true); // specifica che è una post request
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // ritorna il risultato come stringa
+
+        $headers = array(
+            "Content-Type: application/json",
+            "Content-Lenght: 0",
+        );
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers); // setta gli headers della request
+
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+
+        $responseJson = curl_exec($curl);
+
+        curl_close($curl);
+
+        $response = json_decode($responseJson);
+
+        if ($response->response == true)
+        {
+            $_SESSION['user_id'] = $response->userID;
+            header('Location: index.php');
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+?>
